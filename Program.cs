0b44eb6a71eb7561f788be9d0fef7dc67b23ba0b
Daniel Paulino da Silva Falcao <daniel.falcao@havan.com.br>
2022-06-14 08:41:08 -0300

    Injeção de dependência do CalculoService no program

--- Program.cs
+++ Program.cs
@@ -4,6 +4,13 @@ namespace Udemy_Console
 {
     public class Program
     {
+        private readonly CalculoService _CalculoService;
+
+        public Program(CalculoService calculoService)
+        {
+            _CalculoService = calculoService;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
