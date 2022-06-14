58d5e944d34dfe19e9f948e1fd8f595d2d9693f5
Daniel Paulino da Silva Falcao <daniel.falcao@havan.com.br>
2022-06-14 08:37:05 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Udemy_Console
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
